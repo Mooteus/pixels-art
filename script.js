8fe1f9cb3ef9c7216bc03564d9eeeef1e16ad7c7
Mooteus <moteus10@gmail.com>
2022-02-16 19:10:32 -0300

    refatorando codigo

--- script.js
+++
@@ -1,6 +0,0 @@
-let colorSelector = document.getElementsByClassName('color');
-colorSelector[0].style.backgroundColor = 'black';
-colorSelector[1].style.backgroundColor = 'blue';
-colorSelector[2].style.backgroundColor = 'green';
-colorSelector[3].style.backgroundColor = 'red';
-
