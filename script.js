d25404543524a7f34fd1f348152bc68127e510b0
Mooteus <moteus10@gmail.com>
2022-02-16 20:18:57 -0300

    criando funcao renderColors

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function renderColors() {
+  const divColors = document.getElementsByClassName('color');
+  const listColor = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'gray', 'cyan', 'orange'];
+  divColors[0].style.backgroundColor = 'black';
+  for (let i = 1; i < 4; i += 1) {
+    const colorNum = Math.floor(Math.random() * 10) - 2;
+    divColors[i].style.backgroundColor = listColor[colorNum];
+  }
+
+}
+
 function renderColorPalette() {
   const divPalette = document.getElementById('color-palette');
 
@@ -6,6 +17,7 @@ function renderColorPalette() {
     color.classList.add('color');
     divPalette.appendChild(color);
   }
+  renderColors();
 }
 
 renderColorPalette();
