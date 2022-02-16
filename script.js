8a86294f48448a74174861e596a22e7ebd77d757
Mooteus <moteus10@gmail.com>
2022-02-16 17:06:42 -0300

    definindo cor preta como primeira na paleta

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let colorSelector = document.getElementsByClassName('color');
-colorSelector[0].style.backgroundColor = 'red';
+colorSelector[0].style.backgroundColor = 'black';
 colorSelector[1].style.backgroundColor = 'blue';
 colorSelector[2].style.backgroundColor = 'green';
-colorSelector[3].style.backgroundColor = 'black';
+colorSelector[3].style.backgroundColor = 'red';
 
