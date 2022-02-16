307bb17dac0d5fa900a765a86abaac873b0c9a70
Mooteus <moteus10@gmail.com>
2022-02-16 19:27:45 -0300

    adicionando classes nas divs geradas por renderColorPalette

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function renderColorPalette() {
   const divPalette = document.getElementById('color-palette');
   for (let i = 0; i < 4; i += 1) {
     const color = document.createElement('div');
+    color.classList.add('color');
     divPalette.appendChild(color);
   }
 }
