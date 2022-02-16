0b1ceb05bad3a2cf118813780e82a78e585edb08
Mooteus <moteus10@gmail.com>
2022-02-16 17:03:06 -0300

    organizando cores paleta

---
+++ script.js
@@ -0,0 +1,6 @@
+let colorSelector = document.getElementsByClassName('color');
+colorSelector[0].style.backgroundColor = 'red';
+colorSelector[1].style.backgroundColor = 'blue';
+colorSelector[2].style.backgroundColor = 'green';
+colorSelector[3].style.backgroundColor = 'black';
+
