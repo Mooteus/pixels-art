2717515071ef5d908d74f85466e307a5646511ea
Mooteus <moteus10@gmail.com>
2022-02-16 19:30:41 -0300

    adicionando forma as divs criadas

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function renderColorPalette() {
   const divPalette = document.getElementById('color-palette');
+
   for (let i = 0; i < 4; i += 1) {
     const color = document.createElement('div');
     color.classList.add('color');
