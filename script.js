45417f2477cd5b918b19e66ebad73c994f00737d
Mooteus <moteus10@gmail.com>
2022-02-17 11:19:15 -0300

    gerando pixels dentro de pixel-board

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function renderColors() {
   const divColors = document.getElementsByClassName('color');
   const listColor = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'gray', 'cyan', 'orange'];
   divColors[0].style.backgroundColor = 'black';
+
   for (let i = 1; i < 4; i += 1) {
     const colorNum = Math.floor(Math.random() * 10) - 2;
     divColors[i].style.backgroundColor = listColor[colorNum];
   }
-
 }
 
 function renderColorPalette() {
@@ -20,4 +20,15 @@ function renderColorPalette() {
   renderColors();
 }
 
+function renderPixels() {
+  const PixelBoard = document.getElementById('pixel-board');
+
+  for (let i = 0; i < 25; i += 1) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    PixelBoard.appendChild(pixel);
+  }
+}
+
 renderColorPalette();
+renderPixels();
